32bb7a0e23ee4437ad54c6a92daa20b0a9f594f5
Derek Kelly <dkman123@hotmail.com>
2019-05-21 18:58:25 -0400

    initial

---
+++ phpBB3/putFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo cp ./vendor/s9e/text-formatter/src/Configurator.php /var/www/html/phpBB3/vendor/s9e/text-formatter/src/Configurator.php
+sudo cp ./vendor/s9e/text-formatter/src/Utils.php /var/www/html/phpBB3/vendor/s9e/text-formatter/src/Utils.php
+sudo cp ./vendor/s9e/text-formatter/src/Renderers/PHP.php /var/www/html/phpBB3/vendor/s9e/text-formatter/src/Renderers/PHP.php
+sudo cp ./vendor/s9e/text-formatter/src//Plugins/MediaEmbed/Configurator/Collections/XmlFileDefinitionCollection.php /var/www/html/phpBB3/vendor/s9e/text-formatter/src/Plugins/MediaEmbed/Configurator/Collections/XmlFileDefinitionCollection.php
+sudo cp ./vendor/symfony/dependency-injection/Loader/XmlFileLoader.php /var/www/html/phpBB3/vendor/symfony/dependency-injection/Loader/XmlFileLoader.php
+sudo cp ./vendor/symfony/routing/Loader/XmlFileLoader.php /var/www/html/phpBB3/vendor/symfony/routing/Loader/XmlFileLoader.php
